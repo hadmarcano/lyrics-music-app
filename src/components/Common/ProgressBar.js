4019755252387c9ef3cdb677b6b4f56e54fff19e
Hector Adolfo <hadmarcano@gmail.com>
2021-02-03 21:34:12 -0300

    defining static header component

---
+++ src/components/Common/ProgressBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import CircularProgress from "@material-ui/core/CircularProgress";
+
+const ProgressBar = () => (
+  <div>
+    <CircularProgress></CircularProgress>
+  </div>
+);
+
+export default ProgressBar;
